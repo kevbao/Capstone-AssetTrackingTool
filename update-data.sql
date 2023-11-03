bbeb13b062a9e67585a85dca4c09a3aad5c5b84a
Miles Koehnemann <70864211+CCMiles32@users.noreply.github.com>
2023-11-03 13:44:16 -0700

    Add files via upload

---
+++ update-data.sql
@@ -0,0 +1,7 @@
+UPDATE Accessory SET Name = 'UpdatedName' WHERE Accessory_id = 1;
+
+UPDATE Location SET Name = 'UpdatedName' WHERE Location_id = 1;
+
+UPDATE Asset SET Asset_Name = 'UpdatedName' WHERE Asset_ID = 1;
+
+UPDATE Member SET Name = 'UpdatedName' WHERE GD_id = 1;
