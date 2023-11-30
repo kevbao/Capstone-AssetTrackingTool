9fa3365470a4641147f178c7f86940bf7994210a
kevbao <kevinle3@asu.edu>
2023-11-29 18:12:02 -0700

    Add a new asset to the Asset table implemented

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(express.json()); // Parse JSON bodies
+
 const port = 8081
 
 /*in order to get this to work: run these in terminal:
@@ -31,6 +34,41 @@ app.get('/Asset', (req, res) => {
         return res.json(data);
     })
 })
+
+app.post('/addAsset', (req, res) => {
+    const formData = req.body; // Retrieve the entire form data object
+
+    console.log(formData); // Verify if formData is received correctly
+
+    const sql = `INSERT INTO Asset 
+                (Asset_Name, Asset_Tag, VersionHistory, Current_Image, Model, Type, AssetTag, Category, Status, Purchase_Date, Cost, Deployed) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+    const values = [
+        formData.Asset_Name,
+        formData.Asset_Tag,
+        formData.VersionHistory,
+        formData.Current_Image,
+        formData.Model,
+        formData.Type,
+        formData.AssetTag,
+        formData.Category,
+        formData.Status,
+        formData.Purchase_Date,
+        formData.Cost,
+        formData.Deployed
+    ];
+
+    db.query(sql, values, (err, result) => {
+        if (err) {
+            //console.error('Database query error:', err); // Log the specific database query error
+            return res.status(500).json({ error: 'Database query error' });
+        }
+        return res.status(200).json({ message: 'Asset added successfully' });
+    });
+    
+});
+
 
 app.listen(port, ()=> {
     console.log("LISTENING")
